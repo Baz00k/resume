18c652868697c921814e39d31639668dd7fc6b80
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-08-02 16:40:31 +0200

    chore: change layout

---
+++ src/layouts/tokyo/tokyo.svelte
@@ -0,0 +1,9 @@
+<div
+	class="prose dark:prose-invert prose-tokyo px-6 py-8 max-w-full min-h-full bg-tokyo-light dark:bg-tokyo-storm prose-h1:text-center prose-h2:underline prose-headings:font-medium prose-h1:text-[3rem] prose-headings:underline-offset-4 prose-headings:decoration-2 prose-ul:flex prose-ul:flex-wrap prose-ul:gap-x-3 prose-ul:list-none prose-ul:max-w-[40rem] prose-li:bg-gray-400/10 dark:prose-li:bg-gray-100/10 prose-li:rounded prose-li:py-1 prose-li:px-2 prose-li:transition-all"
+>
+	<slot />
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway&display=swap');
+</style>
