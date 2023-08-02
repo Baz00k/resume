da39b80ef7e73cd6214db2f3f65fc48f73e232d0
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-08-02 17:19:37 +0200

    feat: add contact information

---
+++ src/components/Contact.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	interface ContactLink {
+		icon: string;
+		link: string;
+		linkText: string;
+		text: string;
+	}
+
+	const contactLinks: ContactLink[] = [
+		{
+			icon: 'mdi:email',
+			text: 'Email:',
+			link: 'mailto:gustav.buzuk@gmail.com',
+			linkText: 'gustav.buzuk@gmail.com'
+		},
+		{
+			icon: 'mdi:linkedin',
+			text: 'LinkedIn:',
+			link: 'https://www.linkedin.com/in/jakub-buzuk',
+			linkText: '@Jakub Buzuk'
+		},
+		{
+			icon: 'mdi:github',
+			text: 'GitHub:',
+			link: 'https://github.com/Baz00k',
+			linkText: '@Baz00k'
+		}
+	];
+</script>
+
+{#each contactLinks as contactLink}
+	<p class="flex items-center gap-2">
+		<Icon icon={contactLink.icon} />
+		{contactLink.text}
+		<a href={contactLink.link} target="_blank" rel="noopener noreferrer" class="print:no-underline">
+			{contactLink.linkText}
+		</a>
+	</p>
+{/each}
